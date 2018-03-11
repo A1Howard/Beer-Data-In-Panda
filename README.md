8387536186845af015ab7d07838349cd56db31c0
Adam Howard <stalh05@moravian.edu>
2018-03-11 19:44:52 -0400

    Added pie chart capability to project objectives

--- README.md
+++ README.md
@@ -5,10 +5,14 @@
 * Can we predict beer sales?
 * Can we predict inventory shipments?
 * How much beer should they get on certain days?
-* When do they ship in beer?
+* When do they receive beer?
     * Do the different categories also have different shipment days?
 * What day of the week has the greatest quantity available fluctuation?
 * What beer fluctuates the most?
 * Do holidays affect sales?
     * St. Patrick's Day
     * JAC fest
+* One file analysis:
+    * Make pie charts for liquid volume per category
+    * Join a key that defines the style of beer
+    * Any kind of plot
--- panda.py
+++ panda.py
@@ -1,12 +1,76 @@
 
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+from query_inventory import get_available_inventory_dict
+from query_volumes_of_beer import make_volume_object, make_data_frame
 
-filename = 'Inventories/2018-02-28_12:35:40.880574.csv'
-inventory = pd.read_csv(filename)
+filename = '2018-03-11_19:24:51.217556.csv'
 
-inventory = inventory.iloc[:, 0:7]
-# print(inventory.head())
-# print(inventory.corr())
 
-print(inventory.describe())
+def display_beer_categories_by_count(filename):
+    # Data to plot
+    labels = ['Cider', 'Craft', 'Domestic', 'Import', 'Malternative', 'Mead']
+    sizes = []
+    cider_count = 0
+    craft_count = 0
+    domestic_count = 0
+    import_count = 0
+    malt_count = 0
+    mead_count = 0
+    d = get_available_inventory_dict(filename)
+    for item in d['Category']:
+        if item.title() == 'Cider':
+            cider_count += 1
+        elif item.title() == 'Craft':
+            craft_count += 1
+        elif item.title() == 'Domestic':
+            domestic_count += 1
+        elif item.title() == 'Import':
+            import_count += 1
+        elif item.title() == 'Malternative':
+            malt_count += 1
+        elif item.title() == 'Mead':
+            mead_count += 1
+
+    sizes.append(cider_count)
+    sizes.append(craft_count)
+    sizes.append(domestic_count)
+    sizes.append(import_count)
+    sizes.append(malt_count)
+    sizes.append(mead_count)
+
+    colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'red', 'purple']
+
+    # Plot
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+    plt.show()
+
+
+def display_beer_categories_by_volume(filename):
+    labels = ['Cider', 'Craft', 'Domestic', 'Import', 'Malternative', 'Mead']
+    sizes = []
+
+    cider_category = make_volume_object('CIDER')
+    craft_category = make_volume_object('CRAFT')
+    domestic_category = make_volume_object('DOMESTIC')
+    import_category = make_volume_object('IMPORT')
+    malt_category = make_volume_object('MALTERNATIVE')
+    mead_category = make_volume_object('MEAD')
+    make_data_frame(filename)
+
+    sizes.append(cider_category.get_volume())
+    sizes.append(craft_category.get_volume())
+    sizes.append(domestic_category.get_volume())
+    sizes.append(import_category.get_volume())
+    sizes.append(malt_category.get_volume())
+    sizes.append(mead_category.get_volume())
+
+    colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'red', 'purple']
+
+    # Plot
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+    plt.show()
+
+
+display_beer_categories_by_count(filename)
+display_beer_categories_by_volume(filename)
